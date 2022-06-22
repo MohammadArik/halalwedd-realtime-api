78739cbd0161e2198d1906537746c6f1c605d38a
Mohammad <mohammadarik63@gmail.com>
2022-06-22 18:36:27 +0600

    verify server function added and parameters optimized

--- serverConnectionHandler.go
+++ serverConnectionHandler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 
 	serverConnectionService "github.com/MohammadArik/halalwedd/realtime-api/serverConnection"
 )
@@ -10,12 +11,24 @@ type serverConnectionHandler struct {
 	serverConnectionService.UnimplementedServerCheckingServer
 }
 
-func (serverConnectionHandler) PingServer(ctx context.Context, _ *serverConnectionService.CheckReq) (*serverConnectionService.CheckRes, error) {
+func (serverConnectionHandler) PingServer(context.Context, *serverConnectionService.CheckReq) (*serverConnectionService.CheckRes, error) {
 	res := &serverConnectionService.CheckRes{
 		Pong: "PONG",
 	}
 	return res, nil
 }
 
-// VerifyServer(context.Context, *AuthReq) (*AuthRes, error)
+func (serverConnectionHandler) VerifyServer(_ context.Context, req *serverConnectionService.AuthReq) (*serverConnectionService.AuthRes, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error recovered:", r)
+		}
+	}()
+
+	res := &serverConnectionService.AuthRes{
+		Result: decryptCipher(req.Challenge),
+	}
+	return res, nil
+}
+
 // DataUpdate(context.Context, *ServerInfoUpdate) (*DataUpdateRes, error)
