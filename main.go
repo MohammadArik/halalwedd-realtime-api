7b4c162fb6b4a51267451b3ee19b693397b19ae8
Mohammad <mohammadarik63@gmail.com>
2022-06-22 16:04:53 +0600

    AES key loading and class initiating added

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"log"
+	"os"
+)
+
+var aesClass cipher.AEAD
+
+func main() {
+	// //* Log initial data
+	log.Println("Server Initialization Started")
+	log.Println("Server PID:", os.Getpid())
+	//* Load AES keys and initiate AES Block
+	keyFileData, err := os.ReadFile("./keys/key.dat")
+	panicOnErr(err)
+	key, err := hex.DecodeString(string(keyFileData))
+	panicOnErr(err)
+	block, err := aes.NewCipher(key)
+	panicOnErr(err)
+	aesClass, err = cipher.NewGCM(block)
+	panicOnErr(err)
+
+}
